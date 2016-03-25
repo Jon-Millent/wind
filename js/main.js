a544a9320ddccc1d1d7bbeed39a95ccd1a0bf98e
millent <millent@126.com>
2016-03-25 16:07:06 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 window.onload=function(){
-	var thisviedo=document.getElementById('thisvideo');
 	var doc=document.documentElement||docuemnt.body;
 	var zhe=document.getElementById('zhe');
 	var height=doc.scrollHeight;
@@ -9,18 +8,9 @@ window.onload=function(){
 	zhe.style.height=height+"px";
 	zhe.style.width=width+"px";
 	setTimeout(function(){
-		thisvideo.play();
 		zhe.setAttribute('class',nowclass);
 		setTimeout(function(){
 			zhe.style.display="none";
 		},1000)
 	},1500);
-	var mp3=document.getElementById('mp3');
-	mp3.onclick=function(){
-		if(!thisvideo.paused){
-			thisvideo.pause();
-		}else{
-			thisvideo.play();
-		}	
-	}
 }
