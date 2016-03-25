fd19a85d7f7902dc198e5be4f6e5a26945a741d5
millent <millent@126.com>
2016-03-25 15:58:07 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,26 @@
-/*window.onload=function(){
-	var width=document.documentElement.clientWidth||document.body.clientWidth;
-	var center=((2118-width)/2);
-	var bili=center/400;
-	var nav=document.getElementById('nav');
-	var left=document.getElementById('left');
-	var right=document.getElementById('right');
-	var rightIsIn=false;
-	var leftIsIn=false;
-
-	nav.style.left=(-center)+"px";
-	console.log(width)
-	left.onmouseenter=function(){
-		leftIsIn=true;
+window.onload=function(){
+	var thisviedo=document.getElementById('thisvideo');
+	var doc=document.documentElement||docuemnt.body;
+	var zhe=document.getElementById('zhe');
+	var height=doc.scrollHeight;
+	var width=doc.scrollWidth;
+	var norclass=zhe.getAttribute('class');
+	var nowclass=norclass+" opa";
+	zhe.style.height=height+"px";
+	zhe.style.width=width+"px";
+	setTimeout(function(){
+		thisvideo.play();
+		zhe.setAttribute('class',nowclass);
+		setTimeout(function(){
+			zhe.style.display="none";
+		},1000)
+	},1500);
+	var mp3=document.getElementById('mp3');
+	mp3.onclick=function(){
+		if(!thisvideo.paused){
+			thisvideo.pause();
+		}else{
+			thisvideo.play();
+		}	
 	}
-	left.onmouseleave=function(){
-		leftIsIn=false;
-	}
-	left.onmousemove=function(e){
-		if(leftIsIn){
-			nav.style.left=(-center+(400-e.clientX)*bili)+"px";
-		}
-	}
-	right.onmouseenter=function(){
-		rightIsIn=true;
-	}
-	right.onmouseleave=function(){
-		rightIsIn=false;
-	}
-	right.onmousemove=function(e){
-		if(rightIsIn){
-			var leftoff=this.offsetLeft;
-			nav.style.left=(-center-(e.clientX-leftoff))+"px";
-		}
-	}
-}*/
+}
