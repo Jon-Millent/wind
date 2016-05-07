a44bfd344a00aac6afebf17633ffd8107774a56a
millent <html@thisummer.top>
2016-05-07 18:04:44 +0800

    Create circleBar.js

---
+++ children/rose/circleBar/js/circleBar.js
@@ -0,0 +1,70 @@
+(function(window){
+	function circleBar(){
+		this.box='';
+		this.circle='';
+		this.x=0;
+	}
+	circleBar.prototype={
+		constructor:circleBar,
+		init:function(node,callback){
+			this.box=node;
+			this.circle=node.children[0];
+			this.dosome(callback);
+		},
+		dosome:function(callback){
+			this.circle.style.left=-this.circle.clientWidth/2+'px';
+			var This=this;
+			this.on(this.circle,'mousedown',function(e){
+				var e=e||window.event;
+				x=e.clientX-this.offsetLeft;
+				This.on(document,'mousemove',function(e){
+					var e=e||window.event;
+					var tx=e.clientX-x;
+					if(tx<=-This.circle.clientWidth/2){
+						tx=-This.circle.clientWidth/2;
+					}else if(tx>This.box.clientWidth-This.circle.clientWidth/2){
+						tx=This.box.clientWidth-This.circle.clientWidth/2;
+					}
+					This.circle.style.left=tx+'px';
+					callback(This.toChangeNumb(tx+10,This.box.clientWidth));
+				})
+				This.on(document,'mouseup',function(){
+					This.off(document,'mousemove');
+					This.off(document,'mouseup');
+				})
+			})
+		},
+		on:function(node,type,fn){
+			node.lib=node.lib||{};
+			node.lib[type]=node.lib[type]||[];
+			node.lib[type].push(fn)
+			if(node.addEventListener){
+				node.addEventListener(type,fn,false)
+			}else{
+				var tuo=function(){
+					fn.call(node)
+				}
+				node.lib[type].push(tuo)
+				node.attachEvent('on'+type,tuo);
+			}
+		},
+		off:function(node,type){
+			if(node.removeEventListener){
+				for(var i=0;i<node.lib[type].length;i++){
+					node.removeEventListener(type,node.lib[type][i],false);
+				}
+				node.lib[type]=[];
+			}else{
+				for(var i=0;i<node.lib[type].length;i++){
+					node.detachEvent('on'+type,node.lib[type][i]);
+				}
+				node.lib[type]=[];
+			}
+		},
+		toChangeNumb:function(a,b){
+			var ty=(a/b)*100;
+			return Math.floor(ty);
+		}
+	}
+	window.circleBar=circleBar;
+})(window)
